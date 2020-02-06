e9bafe5033c4bcd6850469c73ccaafe574e85fda
Gui <guilhermehenry60@gmail.com>
2020-02-05 21:36:20 -0300

    feat: add appointment model

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      { sequelize }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // Every time a table has more than one relationship with the same
+    // Table you need to pass a nickname with ---> as
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
