2fa977169575e3373452c1a5d51ce824f91c87ff
Gui <guilhermehenry60@gmail.com>
2020-02-08 18:57:08 -0300

    feat: configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '78d1a58a47838a',
+    pass: '6409ee7320e2ec',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
